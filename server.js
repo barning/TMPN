cf5c47f1f26c8ff0d53c34d5d290e0f12de9b953
Niklas <moin@niklasbarning.de>
2015-07-05 22:09:38 +0200

    Fix

--- server.js
+++ server.js
@@ -167,8 +167,7 @@ io.sockets.on('connection', function (socket) {
     var playerNumber = players.length;
 
     if (playerNumber < 2 && !playersReady) {
-      sendAllPlayers('receiver', { msg: 'Du bist alleine! Suche dir mindestens zwei Partner,<br>
-      die mit diesem WiFi verbunden sein müssen.',showbutton:0 });
+      sendAllPlayers('receiver', { msg: 'Du bist alleine! Suche dir mindestens zwei Partner,<br> die mit diesem WiFi verbunden sein müssen.',showbutton:0 });
     }
     else if (!wasIntro) {
       sendAllPlayers('receiver', { msg: 'Ihr seid '+playerNumber+" Personen in diesem WiFi. <br> Seid ihr bereit?",showbutton:1});
